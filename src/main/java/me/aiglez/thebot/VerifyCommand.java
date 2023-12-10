e9a186d940e2279012a36b2455e8990052ffe86a
AIGLEZMA <eaglezledozo@gmail.com>
2023-12-10 20:25:29 +0100

    2

---
+++ src/main/java/me/aiglez/thebot/VerifyCommand.java
@@ -0,0 +1,22 @@
+package me.aiglez.thebot;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class VerifyCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage("This command can only be executed by players.");
+            return false;
+        }
+
+
+
+        return false;
+    }
+}
